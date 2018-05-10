2f2dedf1288a4e876bc7ddb156931bf8f24d2e6d
Nathan <101hawn@users.noreply.github.com>
2018-05-10 13:50:01 +0100

    Added skeleton forms

---
+++ src/main/java/uk/ac/cam/groupseven/weatherapp/screens/CrestScreen.java
@@ -0,0 +1,29 @@
+package uk.ac.cam.groupseven.weatherapp.screens;
+
+import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import uk.ac.cam.groupseven.weatherapp.ScreenLayout;
+
+import javax.swing.*;
+
+public class CrestScreen implements Screen {
+
+    @Override
+    public Disposable start() {
+        //TODO
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public Observable<ScreenLayout.Direction> getScreenChanges() {
+        //TODO
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public JPanel getPanel() {
+        //TODO
+        throw new NotImplementedException();
+    }
+}
---
+++ src/test/java/uk/ac/cam/groupseven/weatherapp/screens/CrestScreenTest.java
@@ -0,0 +1,22 @@
+package uk.ac.cam.groupseven.weatherapp.screens;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import uk.ac.cam.groupseven.weatherapp.mocking.MockedModule;
+
+import javax.swing.*;
+
+public class CrestScreenTest {
+
+    public static void main(String[] args) {
+        Injector injector = Guice.createInjector(new MockedModule());
+        JFrame frame = new JFrame("uk.ac.cam.groupseven.weatherapp.CrestScreenTest");
+        Screen screen = injector.getInstance(CrestScreen.class);
+        screen.start();
+        frame.setContentPane(screen.getPanel());
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+}
