6cab860ae320d1e0a560c1b0c95fb102af582b38
Nathan <101hawn@users.noreply.github.com>
2018-05-10 20:07:27 +0100

    Added crest source

---
+++ src/main/java/uk/ac/cam/groupseven/weatherapp/datasources/CrestSource.java
@@ -0,0 +1,36 @@
+package uk.ac.cam.groupseven.weatherapp.datasources;
+
+import io.reactivex.Observable;
+import uk.ac.cam.groupseven.weatherapp.models.Crest;
+
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.PreferenceChangeListener;
+import java.util.prefs.Preferences;
+
+public class CrestSource {
+    public static final String USER_CREST_PREF = "user_crest";
+    private static final Preferences preferences = Preferences.userNodeForPackage(Crest.class);
+
+    public static void setNewCrest(Crest crest) throws BackingStoreException {
+        preferences.put(USER_CREST_PREF, crest.getCode());
+        preferences.flush();
+    }
+
+    public Observable<Crest> getUserCrests() {
+        return Observable.create(emitter ->
+                {
+                    Crest current = Crest.getCrestFromCode(preferences.get(USER_CREST_PREF, ""));
+                    emitter.onNext(current);
+                    PreferenceChangeListener listener = evt -> {
+                        if (evt.getKey().equals(USER_CREST_PREF)) {
+                            emitter.onNext(Crest.getCrestFromCode(evt.getNewValue()));
+                        }
+                    };
+                    preferences.addPreferenceChangeListener(listener);
+                    emitter.setCancellable(() -> preferences.removePreferenceChangeListener(listener));
+                }
+
+        );
+    }
+
+}
---
+++ src/test/java/uk/ac/cam/groupseven/weatherapp/datasources/CrestSourceTest.java
@@ -0,0 +1,26 @@
+package uk.ac.cam.groupseven.weatherapp.datasources;
+
+import io.reactivex.disposables.Disposable;
+import io.reactivex.observables.ConnectableObservable;
+import org.junit.Assert;
+import org.junit.Test;
+import uk.ac.cam.groupseven.weatherapp.models.Crest;
+
+import java.util.List;
+
+public class CrestSourceTest {
+    @Test
+    public void getUserCrests() throws Exception {
+        CrestSource crestSource = new CrestSource();
+        CrestSource.setNewCrest(Crest.EMMANUEL);
+        ConnectableObservable<Crest> observable = crestSource.getUserCrests().replay();
+        Disposable disposable = observable.connect();
+        CrestSource.setNewCrest(Crest.QUEENS);
+        List<Crest> crests = observable.take(2).toList().blockingGet();
+        Assert.assertEquals(Crest.EMMANUEL, crests.get(0));
+        Assert.assertEquals(Crest.QUEENS, crests.get(1));
+        disposable.dispose();
+
+    }
+
+}
